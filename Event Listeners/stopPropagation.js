9ef7cf147e5b9dfa66e0d4505b25ee81ea178355
Archi Jain <154421998+jainarchi@users.noreply.github.com>
2025-08-01 17:35:25 +0530

    Add files via upload

---
+++ Event Listeners/stopPropagation.js
@@ -0,0 +1,49 @@
+let bt = document.querySelector("#btn");
+let ba = document.querySelector("#a");
+let bb = document.querySelector("#b");
+let bc = document.querySelector("#c");
+let bd = document.querySelector("#d")
+
+// Event Propagation | Event bubbling 
+// when the child was clicked then technically its parents is also clicked (Event listener is fired) then it's parent & so on .
+
+// **stop Propagation is used to stop event propagation
+//  where we add stop prop  - prop stop at that point 
+
+// without stopProp  -> a b - btn - d c 
+//  stop pop add in only bb  -> a b 
+
+
+
+ba.addEventListener("click" , function(e){
+    //  e.stopPropagation();
+    alert("box a was clicked")
+} , true)
+
+bb.addEventListener("click" , function(e){
+    //  e.stopPropagation();
+    alert("box b was clicked")
+}, true)
+
+bc.addEventListener("click" , function(e){    
+    // e.stopPropagation();
+    alert("box c was clicked")
+})
+
+bd.addEventListener("click" , function(e){
+    //  e.stopPropagation();
+    alert("box d was clicked")
+}  )
+
+
+bt.addEventListener("click" , function(e){
+      e.stopPropagation();
+    alert("btn was clicked")
+})
+
+
+
+
+
+
+
