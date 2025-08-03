b68e15ae17284fce56a1247628bdd174752da323
Archi Jain <154421998+jainarchi@users.noreply.github.com>
2025-08-03 16:59:05 +0530

    Update ContactReqCard.js

--- JS Pro/Contact Request Card/ContactReqCard.js
+++ JS Pro/Contact Request Card/ContactReqCard.js
@@ -11,22 +11,22 @@ if (localStorage.getItem("records")) {
     let records = JSON.parse(localStorage.getItem("records"));  // array of array
 
     records.forEach(record => {
-       create_AppendCard(record)
+        create_AppendCard(record)
 
     });
 }
 
 
-function create_AppendCard( fields ) {
+function create_AppendCard(record) {
 
     const card = document.createElement("div");
     card.classList.add("info-card");
 
     card.innerHTML = `
-    <h3>${fields[0]}</h3>
-    <p>Email: ${fields[1]}</p>
-    <p>Phone: ${fields[2]}</p>
-    <p>Category: ${fields[3]}</p>
+    <h3>${record.name}</h3>
+    <p>Email: ${record.email}</p>
+    <p>Phone: ${record.number}</p>
+    <p>Category: ${record.category}</p>
   `;
 
     cardContainer.appendChild(card);
@@ -44,15 +44,20 @@ form.addEventListener("submit", (event) => {
     const category = document.querySelector('input[name="category"]:checked')?.value;
 
 
-    let fields = [name, email, number, category];
-    if (!validateForm(fields)) return;
+    let record = { name, email, number, category };
+    if (!validateForm(record)) return;
 
-    create_AppendCard(fields);
+    create_AppendCard(record);
 
 
     // save in local Storage
     let rec = JSON.parse(localStorage.getItem("records")) || [];
-    rec.push([name, email, number, category])
+    rec.push({
+        name,
+        email,
+        number,
+        category
+    });
     localStorage.setItem("records", JSON.stringify(rec))
 
 
@@ -62,37 +67,36 @@ form.addEventListener("submit", (event) => {
 
 
 
-
 // FORM VALIDATION FUNCTION 
 
-function validateForm(fields) {
+function validateForm(record) {
 
     let isValid = true;
     let errors = [];
 
     // Only letters and spaces, min 2 characters
     const nameRegex = /^[A-Za-z\s]{2,}$/;
-    if (!nameRegex.test(fields[0])) {
+    if (!nameRegex.test(record.name)) {
         isValid = false;
         errors.push("Enter a valid name (only letters, min 2 characters).");
     }
 
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(fields[1])) {
+    if (!emailRegex.test(record.email)) {
         isValid = false;
         errors.push("Enter a valid email address.");
     }
 
     // Phone Number: 10 digits, starts with [6-9]
     const numberRegex = /^[1-9]\d{9}$/;
-    if (!numberRegex.test(fields[2])) {
+    if (!numberRegex.test(record.number)) {
         isValid = false;
         errors.push("Enter a valid 10-digit phone number starting with 6-9.");
     }
 
     // Category Must be selected
-    if (!fields[3]) {
+    if (!record.category) {
         isValid = false;
         errors.push("Select a category.");
     }
