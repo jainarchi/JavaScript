f68e1f11508cf44420b5d69191d6e66ed1cb8b6b
Archi Jain <154421998+jainarchi@users.noreply.github.com>
2025-11-06 00:36:41 +0530

    Add debounce functionality to input event listener
    Implement debounce function for input event handling.

--- Optimization/debounce.js
+++ Optimization/debounce.js
@@ -1,1 +1,27 @@
+// debounce - task occurs after sum specific delay .
+// eg : searching in amazon  , filter after some delay
 
+let input = document.querySelector('input') ;
+
+function debounce( fnc , delay){
+    let timer ;
+
+   return function(...args) {          // ...args is the event object 
+        clearTimeout(timer);
+        timer = setTimeout(()=>{
+            fnc(...args);
+        } , delay );   
+   }
+}
+
+input.addEventListener("input" , 
+    debounce(function(){
+        console.log(`show search result`);
+            
+} , 1000))
+
+
+
+
+// input.addEventListener("input" , debounce() )     
+//  deboune run when the code read without event ocuur - problem 
